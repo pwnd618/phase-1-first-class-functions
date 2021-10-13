d8b6f8924e01dc48d8855c3533e8761d392d2c22
webster <wbian618@gmail.com>
2021-10-13 11:38:16 -0400

    commit tree 132902554fed520b2de0dc78e5bdfe69e659fc74

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(name) {
+    return name()
+}
+
+function returnsANamedFunction() {
+    
+    return function name() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    
+    return function () {
+
+    }
+}
